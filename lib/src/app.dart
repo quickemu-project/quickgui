5afb732f5eef14693bc1bab5ee8070f4e9be5c19
Phil Clifford <philclifford@users.noreply.github.com>
2021-11-15 21:52:10 +0000

    Italian translation (#23)
    * starting italian
    
    * First attempt at Itialian
    
    * front matter tweaks
    
    * Updated pot addressed
    
    * satisfy the inspector
    
    * more little corrections
    
    * Adding it to app.dart supported locale list

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -55,6 +55,7 @@ class _AppState extends State<App> with PreferencesMixin {
                 Locale('en'),
                 Locale('fr'),
                 Locale('fr', 'CH'),
+                Locale('it'),
               ],
               localizationsDelegates: [
                 GettextLocalizationsDelegate(),
