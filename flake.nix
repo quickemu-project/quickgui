84a329b54f394888c38e676bb99af0d5cd35ffa9
Martin Wimpress <martin@wimpress.org>
2024-07-02 01:50:25 +0100

    feat: nix package, overlay and devshell use the quickemu FlakeHub flake

--- flake.nix
+++ flake.nix
@@ -2,37 +2,46 @@
   description = "Quickgui flake";
   inputs = {
     flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*.tar.gz";
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
+    quickemu.url = "https://flakehub.com/f/quickemu-project/quickemu/*.tar.gz";
+    quickemu.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     flake-schemas,
     nixpkgs,
+    quickemu,
   }: let
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        # TODO: Enable when upstream supports these platforms
-        #"aarch64-linux"
-        #"x86_64-darwin"
-        #"aarch64-darwin"
-      ] (system: function nixpkgs.legacyPackages.${system});
+      # Define supported systems and a helper function for generating system-specific outputs
+      #TODO: Add the following as quickemu/quickgui/GitHub builders support them:
+      #      aarch64-darwin aarch64-linux x86_64-darwin
+      supportedSystems = [ "x86_64-linux" ];
+
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        system = system;
+        pkgs = import nixpkgs { inherit system; };
+      });
   in {
-    # Schemas tell Nix about the structure of your flake's outputs
+    # Define schemas for the flake's outputs
     schemas = flake-schemas.schemas;
 
-    overlays.default = final: prev: {
-      quickgui = final.callPackage ./package.nix {};
-    };
+    # Define overlays for each supported system
+    overlays = forEachSupportedSystem ({pkgs, system, ...}: {
+      default = final: prev: {
+        quickgui = final.callPackage ./package.nix { quickemu = quickemu.packages.${system}.default; };
+      };
+    });
 
-    packages = forAllSystems (pkgs: rec {
-      quickgui = pkgs.callPackage ./package.nix {};
+    # Define packages for each supported system
+    packages = forEachSupportedSystem ({pkgs, system, ...}: rec {
+      quickgui = pkgs.callPackage ./package.nix { quickemu = quickemu.packages.${system}.default; };
       default = quickgui;
     });
 
-    devShells = forAllSystems (pkgs: {
-      default = pkgs.callPackage ./devshell.nix {};
+    # Define devShells for each supported system
+    devShells = forEachSupportedSystem ({pkgs, system, ...}: {
+      default = pkgs.callPackage ./devshell.nix { quickemu = quickemu.packages.${system}.default; };
     });
   };
 }
